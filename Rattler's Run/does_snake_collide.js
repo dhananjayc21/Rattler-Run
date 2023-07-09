f00e2012b52917aa0de0975caa4734dbec9f1444
dhananjayc21 <121111449+dhananjayc21@users.noreply.github.com>
2023-07-09 21:54:17 +0530

    Update does_snake_collide.js

--- Rattler's Run/does_snake_collide.js
+++ Rattler's Run/does_snake_collide.js
@@ -6,15 +6,15 @@ function does_snake_collide(snake) {
 
 
     
-     // 1. If snake hits the wall
+     // 1. If the snake hits the wall
 
      if(snake[0].x >= 25 || snake[0].x <=0 || snake[0].y >= 25 || snake[0].y <=0){
         return true;
     }
         
    
-    /* 2. If snake hits his own body element.Basically we will be interating over all the snake body elements starting from 
-          the element present just after the head of snake and continuing till we reach the tail and compare the x and y coorinates 
+    /* 2. If the snake hits his own body element. Basically, we will be iterating over all the snake body elements starting from 
+          the element present just after the head of the snake and continuing till we reach the tail and compare the x and y coordinates 
           of this element with the coordinates of the head.*/ 
 
     for (let i = 1; i < snakeArr.length; i++) {
